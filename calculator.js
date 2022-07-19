86b122b5c7c27b97730fe805ec19c325b7bebb58
Stanley Delva Junior <juniordelva1@gmail.com>
2022-07-19 09:38:14 -0400

    need to start from scracth because of bugs

--- calculator.js
+++ calculator.js
@@ -149,7 +149,7 @@ buttonNumbers.forEach((button) => {
         num2 = Number(e.target.dataset.key);
         emptyArray.push(num2);
         num2 = +emptyArray.join("");
-        console.log(num2);
+        console.log(num1, num2);
       }
 
       displayBox.textContent += Number(e.target.dataset.key);
@@ -157,39 +157,8 @@ buttonNumbers.forEach((button) => {
   });
 });
 
-// creating function
-
-// we need to find a way to populate the operate function
-// for loop might be key
-
-console.log(operate("-", 7, 5));
-console.log(operate(7, "+", 5));
-// const expression = "2+4";
-// const res = Function("return " + expression)();
-// console.log(res);
-
 // operate();
 // Function takes a string or a number expression and evaluate it
 // let result = displayBox.textContent;
 // let finalResult = Function("return " + result)();
 // displayBox.textContent = finalResult;
-function testingArray(string) {
-  let array = string.split("");
-  let num1, num2, operator;
-
-  for (let i = 0; i < array.length; i++) {
-    if (isNaN(+array[i]) === false) {
-      num1 = +array[i];
-    } else if (isNaN(+array[i]) === true) {
-      operator = array[i];
-    }
-
-    if (typeof num1 === "number") {
-      // num2 = array[i];
-    }
-    // console.log(isNaN(+array[i]));
-  }
-  return [num1, operator, num2, "IIF works"];
-}
-
-// console.log(isNaN(+"1"));
